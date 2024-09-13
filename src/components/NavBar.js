3e9ec37f15c736ee9a1b844f29a15b08cc8473cb
venkateshramkumarg <venkateshramkumar3@gmail.com>
2024-09-13 12:29:10 +0530

    Refactor NavBar component to add logout functionality and improve styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
 
 function NavBar() {
     const navBars = [
@@ -7,22 +7,34 @@ function NavBar() {
         {path:'/write',element:'Write'},
         {path:'/search',element:'Search'}
     ];
+    const navigate=useNavigate();
+
+    const handleClick = () => { 
+        sessionStorage.removeItem('sessionStorage');
+        navigate('/');
+    }
 
     const location = useLocation();
 
     const getLinkClassName = (path) => location.pathname === path ? 'text-blue-500' : 'text-black';
 
     return (
-        <div className='sm:max-w-2xl mx-auto mt-5 md:mb-5 max-w-md'>
-            <ul className='flex gap-5 items-center'>
-                {navBars.map((item) => (
-                    <li key={item.path} className='font-semibold text-xl font-serif'>
-                        <Link to={item.path} className={getLinkClassName(item.path)}>
-                            {item.element}
-                        </Link>
-                    </li>
-                ))}
-            </ul>
+        <div className='sm:max-w-2xl mx-auto mt-5 md:mb-5 max-w-md flex justify-between items-center'>
+            <div>
+                <ul className='flex justify-center space-x-4'>
+                    {navBars.map((navBar, index) => (
+                        <li key={index}>
+                            <Link to={navBar.path} className={`text-xl font-semibold font-mono ${getLinkClassName(navBar.path)}`}>
+                                {navBar.element}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+            <div>
+                <button className='bg-blue-500 text-white px-2 py-1 rounded-md' onClick={handleClick}>LogOut</button>
+            </div>
+
         </div>
     );
 }
